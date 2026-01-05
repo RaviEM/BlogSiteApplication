3cc305f79506e79c4c3257c12dfedbdc72c04f6c
Ravi EM <raviemindia@gmail.com>
2026-01-06 01:09:58 +0530

    updates

--- blog-common/src/main/java/com/blogsite/blog_common/model/entity/User.java
+++ blog-common/src/main/java/com/blogsite/blog_common/model/entity/User.java
@@ -9,10 +9,6 @@ import lombok. AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +17,8 @@ import java.util.List;
  * Stored in MongoDB database.
  * Used for tagging and filtering blog posts.
  */
-@Document(collection = "users")
+@Entity
+@Table(name = "users")
 @Data
 @Builder
 @NoArgsConstructor
